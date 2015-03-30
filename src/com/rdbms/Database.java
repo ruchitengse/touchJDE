21fd170789672b0ddb63a38ddf82e9a6433ae67b
ruchitengse <ruchitengse@gmail.com>
2015-03-29 23:57:09 -0500

    Database class to retrieve connection

---
+++ src/com/rdbms/Database.java
@@ -0,0 +1,44 @@
+package com.rdbms;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.properties.ReadProperties;
+
+public class Database {
+	
+	private static Connection connection = null;
+    private static String connectionString = null;
+	
+    public static Connection getConnection(){
+        
+        if(connectionString == null){
+            setConnectionString();
+        }
+        try {
+                Class.forName("com.mysql.jdbc.Driver");
+                connection = DriverManager.getConnection(connectionString);
+                connection.setAutoCommit(false);
+        } catch (ClassNotFoundException | SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+        }
+        return connection;
+    }
+
+    private static void setConnectionString() {
+        connectionString = ReadProperties.getInstance().getValue("DATABASE_SERVER") + "?user=" + ReadProperties.getInstance().getValue("DATABASE_USER") + "&password=" + ReadProperties.getInstance().getValue("DATABASE_PASSWORD");
+    }
+	
+    public void closeConnection(){
+        if(connection != null){
+                try {
+                        connection.close();
+                } catch (SQLException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                }
+        }
+    }
+}
