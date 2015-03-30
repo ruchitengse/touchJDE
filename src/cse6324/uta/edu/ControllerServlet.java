7b57d0f7ea2eec348028e6f8c932c761168c7aa8
ruchitengse <ruchitengse@gmail.com>
2015-03-30 00:39:19 -0500

    Updated annotation @Servlet

--- src/cse6324/uta/edu/ControllerServlet.java
+++ src/cse6324/uta/edu/ControllerServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ControllerServlet
  */
-@WebServlet("*.action")
+@WebServlet(name="ControllerServlet", urlPatterns={"*.action"})
 public class ControllerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
