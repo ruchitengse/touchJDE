a531a104b8f1be122fe0595dce6d115ad3b3b31f
ruchitengse <ruchitengse@gmail.com>
2015-05-03 01:00:18 -0500

    Added comments

--- src/com/web/servlet/CompileProgram.java
+++ src/com/web/servlet/CompileProgram.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.gson.Gson;
 
 /**
- * Servlet implementation class CompileProgram
+ * This servlet handles the request for compiling and running a program
  */
 
 @WebServlet(name="CompileProgram", urlPatterns={"/CompileProgram"})
@@ -28,6 +28,9 @@ public class CompileProgram extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+    /**
+     * doGet not used since it is not 
+     */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
@@ -38,18 +41,32 @@ public class CompileProgram extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		StringBuilder javaClassCode = new StringBuilder();
-		String classCode = request.getParameter("create");
+		String classCode = request.getParameter("create"); 				//The content present in the text area
 		String res = "";
+		
+		//Check if text area is empty
 		if("".equals(classCode.trim())){
-			res = "No code found to compile";
+			res = "No code found to compile"; 
 		} else {
+			
+			//Append class code to StringBuilder
 			javaClassCode.append(classCode);
 			String pkgName = "";
+			
+			//Check if the class is present in a package
 			if(javaClassCode.toString().startsWith("package")){
 				pkgName = javaClassCode.toString().split(";")[0].replaceAll("package ", "");
 			}
+			
+			/**Call compileAndRunProgram from the JavaCompile class
+			 * Get the response of the function back
+			 */
 			res = JavaCompile.compileAndRunProgram(javaClassCode.toString(), pkgName, request.getParameter("className"));
 		}
+		
+		/**
+		 * Convert response to json and print on the web page
+		 */
 		String json = new Gson().toJson(res);
 		response.setContentType("application/json");
 		response.setCharacterEncoding("UTF-8");
--- src/com/web/servlet/JavaCompile.java
+++ src/com/web/servlet/JavaCompile.java
@@ -5,18 +5,24 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.regex.Pattern;
-
 import com.properties.ReadProperties;
 
 
 public class JavaCompile {
 	
+	//Properties needed from touchjde.properties for compiling and running
 	private static String javaFileLocation = ReadProperties.getInstance().getValue("FILE_STORAGE_DIRECTORY");
 	private static String javaCompilerPath = ReadProperties.getInstance().getValue("JAVA_HOME") + System.getProperty("file.separator") + "bin" + System.getProperty("file.separator") + "javac";
 	private static String javaRunPath = ReadProperties.getInstance().getValue("JAVA_HOME") + System.getProperty("file.separator") + "bin" + System.getProperty("file.separator") + "java";
 	
 	
+	/**
+	 * Function which creates a java class file, compiles the code, and runs it with the help of standard java commands javac and java
+	 * @param javaCode: The java code written in text area
+	 * @param pkgName: The package where the java code is to be stored
+	 * @param className: The name of  the class
+	 * @return The response (Output of running the program/Compile error/Exception while running)
+	 */
 	public static String compileAndRunProgram(String javaCode, String pkgName, String className){
 		
 		String newPackageName = "";
@@ -24,12 +30,22 @@ public class JavaCompile {
 			newPackageName = pkgName.replace(".", System.getProperty("file.separator"));
 		}
 		
+		//Call createFile function, if function returns true file is created, else file is not created
 		boolean fileCreated = createFile(javaCode, newPackageName, className);
+		
 		if(fileCreated){
+			//If file is created, compile the java code
 			String response = compileProgram(javaCode, newPackageName, className);
+			
+			//Compile program returns empty string file compiled successfully
 			if("".equals(response)){
+				//If file compiled successfully, add "Success" to response
 				response = "The code compiled successfully. <br>";
+				
+				//Run the file now to get the output
 				String runResponse = runProgram(javaCode, pkgName, className);
+				
+				//If the file which is run has no output to display, add the appropriate response to "response" string, else append the output
 				if("".equals(runResponse)){
 					response += "No output to display. <br>";
 				} else {
@@ -37,14 +53,24 @@ public class JavaCompile {
 					response += runResponse;
 				}
 			} else {
+				//If the file could not compile, specify appropriate message
 				response = "The file could not compile <br>" + response;
 			}
+			
+			//return the response back to servlet
 			return response;
 		} else {
-			return "An error occured. Could not create file. <br>";
+			return "An error occured. Could not create file. Check if the file already exists<br>";
 		}
 	}
 	
+	/**
+	 * Function to create the specified java file
+	 * @param javaCode: The java code to be written to file
+	 * @param pkgName: The package i.e directories to be created for a file
+	 * @param className: The name the file
+	 * @return
+	 */
 	private static boolean createFile(String javaCode, String pkgName, String className){
 		
 		String javaFilePath = javaFileLocation + pkgName;
@@ -60,10 +86,14 @@ public class JavaCompile {
 			try {
 				String filePath = javaFilePath + System.getProperty("file.separator") + className + ".java";
 				f = new File(filePath);
-				FileWriter fw = new FileWriter(f);
-				fw.write(javaCode);
-				fw.close();
-				fileCreated = true;
+				if(f.exists()){
+					fileCreated = false;
+				} else {
+					FileWriter fw = new FileWriter(f);
+					fw.write(javaCode);
+					fw.close();
+					fileCreated = true;
+				}
 			} catch(IOException e){
 				e.printStackTrace();
 				fileCreated = false;
@@ -72,6 +102,13 @@ public class JavaCompile {
 		return fileCreated;
 	}
 
+	/**
+	 * Run the program with the "java <CLASSNAME>" command
+	 * @param javaCode: The java code to be written
+	 * @param pkgName: The package name
+	 * @param className: The classname of the code
+	 * @return The output of run program, empty if no output
+	 */
 	private static String runProgram(String javaCode, String pkgName, String className) {
 		
 		StringBuilder response = new StringBuilder();
@@ -83,6 +120,8 @@ public class JavaCompile {
 		javaFileCommand += className;
 		try {
 			String command = "\"" + javaRunPath + "\"" + " " + "\"" + javaFileCommand + "\"";
+			
+			//Use processbuilder to run "java command", it emulates the dos command prompt
 			ProcessBuilder p = new ProcessBuilder(command);
 			p.directory(new File(javaFileLocation));
             Process proc = p.start();
@@ -122,6 +161,13 @@ public class JavaCompile {
 		return response.toString();
 	}
 
+	/**
+	 * Run the program with the "java <CLASSNAME>" command
+	 * @param javaCode: The java code to be written
+	 * @param pkgName: The package name
+	 * @param className: The classname of the code
+	 * @return The output of compile program, empty if no error, else return the error
+	 */
 	public static String compileProgram(String javaCode, String pkgName, String className) {
 		
 		StringBuilder response = new StringBuilder();
@@ -129,6 +175,8 @@ public class JavaCompile {
 		String classPath = javaFileLocation + pkgName + System.getProperty("file.separator") + className + ".java";
 		try {
 			String command = "\"" + javaCompilerPath + "\"" + " " + "\"" + classPath + "\"";
+			
+			//Use processbuilder to run "java command", it emulates the dos command prompt
             Process p = new ProcessBuilder(command).start();
             stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
             stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
@@ -140,8 +188,7 @@ public class JavaCompile {
             while((s = stdError.readLine()) != null){
                 response.append(s);
                 response.append("<br>");
-            }
-	            
+            }   
 		} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -163,6 +210,6 @@ public class JavaCompile {
 					}
 				}
 			}
-		return response.toString();
+			return response.toString();
 	}
 }
